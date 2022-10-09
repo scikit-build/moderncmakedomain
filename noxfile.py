d6c8c658b7d24f06700dd8d4d4776cb46159ff8c
Henry Schreiner <HenrySchreinerIII@gmail.com>
2022-10-08 20:42:28 -0400

    fix: drop bumpversion and fix a typo, try some basic smoke checks

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,9 @@
 import nox
 import urllib.request
 
-nox.options.sessions = ["lint"]
+nox.options.sessions = ["lint", "tests"]
 
+ALL_PYTHONS = ["3.6", "3.7", "3.8", "3.9", "3.10", "3.11"]
 
 @nox.session
 def lint(session: nox.Session) -> None:
@@ -38,6 +39,7 @@ def update(session: nox.Session) -> None:
         version = session.run(
             "lastversion", "kitware/cmake", log=False, silent=True
         ).strip()
+        session.log(f"CMake {version}")
 
     cmake_url = f"https://raw.githubusercontent.com/Kitware/CMake/v{version}/Utilities/Sphinx/cmake.py"
     colors_url = f"https://raw.githubusercontent.com/Kitware/CMake/v{version}/Utilities/Sphinx/colors.py"
@@ -45,7 +47,11 @@ def update(session: nox.Session) -> None:
     urllib.request.urlretrieve(cmake_url, "sphinxcontrib/moderncmakedomain/cmake.py")
     urllib.request.urlretrieve(colors_url, "sphinxcontrib/moderncmakedomain/colors.py")
 
-    session.install("bump2version")
-    session.run(
-        "bumpversion", "--allow-dirty", "--new-version", version, "patch", "setup.cfg"
-    )
+
+@nox.session(python=ALL_PYTHONS)
+def tests(session):
+    """
+    Run the unit and regular tests.
+    """
+    session.install(".", "pytest")
+    session.run("pytest", *session.posargs)
